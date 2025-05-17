8b60a4c5d787aaf46020851f5b81556ebd8bb3d8
feel2174 <feel2174@gmail.com>
2025-05-17 10:48:06 +0900

    google naver

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9196149361612087"
+          async
+          strategy="beforeInteractive"
+        />
+        <meta name="google-site-verification" content="ylRZwQXQH9ZVegPDqDJGKHanYBIwb2fDMD_NWF917FI" />
+        <meta name="naver-site-verification" content="d81ba3b0f2a3d7fd68ec5938c9d9d8a2e8ef9c65" />
+      </head>
       <body className={inter.className}>
         {children}
         <footer className="bg-gray-50 py-6 mt-8">
